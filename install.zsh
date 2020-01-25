6bd60e79cc36b842cc0fe304d91c039d0c3551e9
mlunax <mlunaelumen@gmail.com>
2020-01-25 01:57:35 +0100

    add update arg to install.sh

--- install.zsh
+++ install.zsh
@@ -1,5 +1,13 @@
 #!/usr/bin/env zsh
 # env, because some OSes keep zsh in /bin (I'm looking at you, Alpine)
+if [[ "$1" == "update" ]]; then
+  cp .zshrc $HOME
+  if [ -d "${HOME}/.zsh" ]; then
+    rm -r $HOME/.zsh
+  fi
+  cp -r .zsh $HOME
+  exit
+fi
 cp .zshrc $HOME
 if [ ! -d "${HOME}/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
