8ebe5fa5883be59f0b500170cefafd5e1cc3efe9
lunax <mlunaelumen@gmail.com>
2020-01-27 12:18:46 +0100

    fix flutter path

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,3 +1,3 @@
 # Flutter
 source $OTHER/env.zsh
-grep -q 'flutter' <<< $devTools && export PATH="$PATH:$HOME/development/flutterxd/bin"
+grep -q 'flutter' <<< $devTools && export PATH="$PATH:$HOME/.development/flutter/bin"
