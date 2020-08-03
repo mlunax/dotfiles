f0dc5975762e323443044e8650b093555c934e08
mlunax <mlunaelumen@gmail.com>
2020-08-03 20:22:49 +0200

    fix: add /bin in npm directory

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -3,5 +3,5 @@ source $OTHER/env.zsh
 grep -q 'flutter' <<< $devTools && export PATH="$PATH:$HOME/.development/flutter/bin"
 grep -q 'dotnet' <<< $devTools && export PATH="$PATH:$HOME/.dotnet"
 # https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
-grep -q 'npm' <<< $devTools && export PATH="$PATH:$HOME/.npm-global"
+grep -q 'npm' <<< $devTools && export PATH="$PATH:$HOME/.npm-global/bin"
 grep -q 'go' <<< $devTools && export PATH="$PATH:/usr/local/go/bin:$(/usr/local/go/bin/go env GOPATH)/bin" 
