ae1dce61cf8854d7db12136f5a31024cb5aadf54
mlunax <lunax@mlunax.com>
2020-09-24 01:22:24 +0200

    feat: simple bash script to intalling vscodium and extention to it

---
+++ install-code.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /etc/lsb-release
+if  [ $# -eq 1 ] && [ $1 == "-h" ]; then
+    echo https://github.com/tonsky/FiraCode/wiki/VS-Code-Instructions
+    exit
+fi
+if [ 0 -eq $(id -u)  ]; then
+    echo Don\'t run this script as root, please.
+    echo If it will need permission it will use 'sudo' command.
+else 
+    if [ $DISTRIB_ID == "ManjaroLinux" ]; then
+        sudo pacman -S ttf-fira-code
+        yay -S vscodium-bin
+        code --install-extension akamud.vscode-theme-onedark
+        code --install-extension vscode-icons-team.vscode-icons
+    fi
+fi
