a9c21abd0ffa56e61520c550841f0a8deccfe77f
mlunax <lunax@mlunax.com>
2020-09-24 20:42:58 +0200

    Squashed commit of the following:
    commit f05701172bca05bc8dd7d798b5a8d3cf33cd23f9
    Author: lunax <mlunaelumen@gmail.com>
    Date:   Thu Sep 24 16:22:09 2020 +0200
    
        1

---
+++ install-packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /etc/lsb-release
+if  [ $# -eq 1 ] && [ $1 == "-h" ]; then
+    exit
+fi
+if [ 0 -eq $(id -u)  ]; then
+    echo Don\'t run this script as root, please.
+    echo If it will need permission it will use 'sudo' command.
+else
+    if [ $DISTRIB_ID == "ManjaroLinux" ]; then
+	      echo Install bat
+        sudo pacman -S bat
+	      echo Install kak
+	      sudo pacman -S kakoune
+    fi
+fi
+
