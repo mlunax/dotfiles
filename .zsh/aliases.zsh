96b3aeed56cd9895fd5d770390e2626d5fe2a073
Luna Komorebi <lunax@mlunax.com>
2021-05-03 01:47:28 +0200

    change discord-stream alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -11,5 +11,5 @@ alias \
     c_buildkit="COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1" \
     ytv="youtube-dl -f bestvideo" \
     smpv="mpv --profile=svp --script-opts=socketPath=/tmp/mpvsocket " \
-    discord-stream="doas modprobe v4l2loopback video_nr=15; nohup ffmpeg -f x11grab -r 30 -s 1920x1080 -i :0+1440,0 -pix_fmt yuv420p -f v4l2 /dev/video15&; disown" \
+    discord-stream="doas modprobe v4l2loopback video_nr=15;nohup ffmpeg -hide_banner -loglevel error -f x11grab -r 30 -s 1920x1080 -i :0+1440,0 -pix_fmt yuv420p -f v4l2 /dev/video15&; disown" \
     vim="nvim"
