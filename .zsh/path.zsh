937bbf9e7acf979489e0dbbb6e6b2eaafa712ca3
Luna Komorebi <lunax@mlunax.com>
2021-05-08 21:23:49 +0200

    change cargo path

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,7 +1,7 @@
 source $OTHER/env.zsh
 grep -q 'flutter' <<< $devTools && export PATH="$PATH:$HOME/.local/opt/flutter/bin"
 grep -q 'dotnet' <<< $devTools && export PATH="$PATH:$HOME/.dotnet"
-grep -q 'cargo' <<< $devTools && source $HOME/.cargo/env
+grep -q 'cargo' <<< $devTools && export PATH="$PATH:$HOME/.cargo/bin"
 grep -q 'gem' <<< $devTools && export PATH="$PATH:$(ruby -e 'puts Gem.user_dir')/bin"
 # https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
 grep -q 'npm' <<< $devTools && export PATH="$PATH:$HOME/.local/npm/bin"
