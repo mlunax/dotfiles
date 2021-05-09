79ed9c71013c73f51b68cf963a55a103654bd652
Luna Komorebi <lunax@mlunax.com>
2021-05-09 04:14:00 +0200

    mpv config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -14,4 +14,4 @@ alias \
     discord-stream="doas modprobe v4l2loopback video_nr=15;nohup ffmpeg -hide_banner -loglevel error -f x11grab -r 30 -s 1920x1080 -i :0+1440,0 -pix_fmt yuv420p -f v4l2 /dev/video15&; disown" \
     screen-stream="discord-stream" \
     vim="nvim" \
-    swap-ytmpv-profiles="sed -i 's/mpv-yt/mpv-yt-inactive/g' ~/.config/mpv/mpv.conf && sed -i 's/mpv-yt-inactive-inactive/mpv-yt/g' ~/.config/mpv/mpv.conf" 
+    swap-ytmpv-profiles="sed -i 's/mpv-yt/mpv-yt-inactive/g' ~/.config/mpv/mpv.conf && sed -i 's/mpv-yt-inactive-inactive/mpv-yt/g' ~/.config/mpv/mpv.conf; grep -1 mpv-yt ~/.config/mpv/mpv.conf" 
