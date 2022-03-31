6a2651eb32b8dc745e29d229312cf61aa2b46e8f
Luna Komorebi <lunax@mlunax.com>
2022-03-31 21:21:47 +0200

    change alias of docker compose

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias \
     wget='wget -c' \
     zshrc='source ~/.zshrc' \
     dps='docker ps -a --format "table {{.Names}}\t{{.Image}}\t{{.Status}}"' \
-    c='docker-compose' \
+    c='docker compose' \
     lla='ls -lha' \
     cp='cp -iv' \
     rm='rm -iv' \
