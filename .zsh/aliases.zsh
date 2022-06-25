f07c823191051d3f1f6e0dd2c194386c50dac8e4
Luna Komorebi <lunax@mlunax.com>
2022-06-25 14:36:47 +0200

    add pyenv and git aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -15,3 +15,14 @@ alias \
     screen-stream="discord-stream" \
     vim="nvim" \
     swap-ytmpv-profiles="sed -i 's/mpv-yt/mpv-yt-inactive/g' ~/.config/mpv/mpv.conf && sed -i 's/mpv-yt-inactive-inactive/mpv-yt/g' ~/.config/mpv/mpv.conf; grep -1 mpv-yt ~/.config/mpv/mpv.conf" 
+    ga='git add' \
+    gap='git add --patch' \
+    gc='git commit' \
+    gd='git diff' \
+    gdc='git diff --cached' \
+    gl='git lg' \
+    gp='git push' \
+    gres='git restore' \
+    gs='git status' \
+    gst='git stash' 
+
