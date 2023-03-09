205f31f5826384a7b2f57ad9ddb9da0c1fc26665
Luna Komorebi <lunax@mlunax.com>
2023-03-10 00:41:18 +0100

    add pulumi

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,8 +1,10 @@
 # Set Devs Tools
-# [] = {flutter, go, dotnet, npm, cargo, gem, android}
+# [] = {flutter, go, dotnet, npm, cargo, gem, android, pulumi}
 export devTools=""
 # Uncomment if u want this env:
 # export GOPATH=""
 
 # Uncomment if u want this env (default: $HOME/.local/npm"):
 # export NPM_CONFIG_PREFIX=""
+
+#export BROWSER="google-chrome-stable"
