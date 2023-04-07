b2b12f7390e3f4d8cc7fb682fc17715233c4a426
Luna Komorebi <lunax@mlunax.com>
2023-04-08 00:52:10 +0200

    Add verbose output for installing OMZsh and powerlevel10k, delete old .zsh/ directory, and install executables

--- install.zsh
+++ install.zsh
@@ -8,15 +8,17 @@ install() {
 
 cp -v .zshrc $HOME
 if [ ! -d "${HOME}/.oh-my-zsh" ]; then
-  echo "Installing OMZsh"
+  echo "[*] Installing OMZsh"
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 if [ ! -d "${HOME}/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
-  echo "Installing powerlevel10k"
+  echo
+  echo "[*] Installing powerlevel10k"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 fi
 if [ -d "${HOME}/.zsh" ]; then
-  echo "Deleting old .zsh/"
+  echo
+  echo "[*] Deleting old .zsh/"
   rm -rf $HOME/.zsh
 fi
 cp -rv .zsh/ $HOME
@@ -34,7 +36,9 @@ if [ -n "${NPM_CONFIG_PREFIX}" ]; then
 fi
 cp -v .tmux.conf ~/.tmux.conf 
 
+echo 
 echo "[*] installing executables"
+
 find bin -type f | while read -r file; do
 	install $file $(echo $file | sed "s|bin|$HOME/.local/bin|")
 done
