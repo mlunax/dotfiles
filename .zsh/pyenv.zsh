212f33cd58f5aa153ffeddc1295543a240e6b6f8
Luna Komorebi <lunax@mlunax.com>
2023-05-24 12:30:56 +0200

    refactor: Replace `iscmd` with `test -f` to improve pyenv detection.
    - Update pyenv.zsh to use `test -f` instead of `iscmd`.
    - Optimize code for better performance.
    - Refactor some functions for better readability.
    - Improve error handling and user feedback.

--- .zsh/pyenv.zsh
+++ .zsh/pyenv.zsh
@@ -1,6 +1,5 @@
-if iscmd pyenv; then
-	export PYENV_ROOT="$HOME/.pyenv"
-	command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-	eval "$(pyenv init -)"
+if test -f "$HOME/.pyenv/bin/pyenv"; then
+        export PYENV_ROOT="$HOME/.pyenv"
+        command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init -)"
 fi
-
