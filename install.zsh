caa498edcb46c1f3d985075d83742e81dba1ea37
Luna <luna@lunax.dev>
2025-09-14 17:08:47 +0200

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -73,7 +73,6 @@ fi
 if ! { [ -f "$HOME"/.ssh/authorized_keys ] && grep -q patrycja "$HOME"/.ssh/authorized_keys }; then
 	echo "[*] installing SSH keys"
 	mkdir -p "$HOME"/.ssh
-	curl -L https://ptrc.gay/keys >> "$HOME"/.ssh/authorized_keys
 fi
 
 os_id="$( . /etc/os-release 2>/dev/null && echo "$ID" || echo "unknown" )"
