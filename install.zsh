2779fae5669451d7df60e7304d24c153b3bd5a71
lunax <lunax@mlunax.com>
2025-09-14 14:04:46 +0200

    rewrite the install because of update and macos

--- install.zsh
+++ install.zsh
@@ -1,21 +1,29 @@
-#!/usr/bin/env zsh
-# env, because some OSes keep zsh in /bin (I'm looking at you, Alpine)
+# stolen from https://github.com/ptrcnull/dotfiles/blob/master/install.zsh
+# love you ptrc for your work <3
+
+#!/bin/zsh
 
 if [ "$ZSH_EVAL_CONTEXT" = "toplevel" ]; then
 	echo "[!] this script is meant to be sourced"
-	echo "source install.zsh" > /dev/stdin
-	exit 1
 fi
 
-iscmd() {
-        command -v "$1" > /dev/null
-}
+plugins_dir="$HOME/.local/share/zsh-plugins"
+plugins_alpine="
+	starship
+	zsh-syntax-highlighting
+	zsh-autosuggestions
+	zsh-completions
+"
+plugins_git="
+	https://github.com/zsh-users/zsh-syntax-highlighting
+	https://github.com/zsh-users/zsh-autosuggestions
+"
 
 for cmd in curl git sed install find; do
-  if ! command -v $cmd >/dev/null; then
-    echo "[!] $cmd not found"
-    exit 1
-  fi
+	if ! command -v $cmd >/dev/null; then
+		echo "[!] $cmd not found"
+		exit 1
+	fi
 done
 
 set -e
@@ -33,86 +41,87 @@ if [ -d "$HOME"/.zsh-custom ]; then
 	rmdir "$HOME"/.zsh-custom
 fi
 
-install() {
-  # screw you coreutils install and your ugly messages
-  command install -Dv $@ | grep -v removed
-}
+if command -v rsync >/dev/null; then
+	echo "[*] installing config files"
+	rsync --archive --update --hard-links --itemize-changes --delay-updates \
+		./config/ ~/.config/
 
-plugins_dir="$HOME/.local/share/zsh-plugins"
-plugins_git="
-  https://github.com/zsh-users/zsh-syntax-highlighting
-  https://github.com/zsh-users/zsh-autosuggestions
-"
+	echo "[*] installing executables"
+	rsync --archive --update --hard-links --itemize-changes --delay-updates \
+		./bin/ ~/.local/bin/
+else
+	install() {
+		command install -dv "$(dirname "$2")" || true
+		# screw you coreutils install and your ugly messages
+		command install -v $@ | grep -v removed
+	}
 
-os_id="$( . /etc/os-release && echo "$ID" )"
+	echo "[*] installing config files"
+	install -m644 .zshrc "$HOME"/.zshrc
+	find config -type f | while read file; do
+		install $file $(echo $file | sed "s|config|$HOME/.config|")
+	done
 
-if [ "$(id -u)" != 0 ]; then
-    if command -v doas >/dev/null; then
-      elevate=doas
-    elif command -v sudo >/dev/null; then
-      elevate=sudo
-    else
-      echo "[!] cannot install zsh plugins system-wide"
-      elevate=:
-    fi
+	echo "[*] installing executables"
+	find bin -type f | while read file; do
+		install $file $(echo $file | sed "s|bin|$HOME/.local/bin|")
+	done
+
+	unfunction install
 fi
 
-cp -v .zshrc $HOME
-if [ -d "${HOME}/.zsh" ]; then
-  echo
-  echo "[*] Deleting old .zsh/"
-  rm -rf $HOME/.zsh
-fi
-cp -rv .zsh/ $HOME
-if [ -n "${devTools}" ]; then
-  sed -i "s%export devTools=\"\"%export devTools=\"$devTools\"%g" $HOME/.zsh/env.zsh
-fi
-if [ -n "${GOPATH}" ]; then
-  sed -i "s%\# export GOPATH=\"\"%export GOPATH=\"$GOPATH\"%g" $HOME/.zsh/env.zsh
-fi
-if [ -n "${NOTICA_URL}" ]; then
-  sed -i "s%\# export NOTICA_URL=\"\"%export NOTICA_URL=\"$NOTICA_URL\"%g" $HOME/.zsh/env.zsh
-fi
-if [ -n "${ANDROID_SDK_ROOT}" ]; then
-  sed -i "s%\# export ANDROID_SDK_ROOT=\"\"%export ANDROID_SDK_ROOT=\"$ANDROID_SDK_ROOT\"%g" $HOME/.zsh/env.zsh
-fi
-if [ -n "${NPM_CONFIG_PREFIX}" ]; then
-  sed -i "s%\# export NPM_CONFIG_PREFIX=\"\"%export NPM_CONFIG_PREFIX=\"$NPM_CONFIG_PREFIX\"%g" $HOME/.zsh/env.zsh
+if ! { [ -f "$HOME"/.ssh/authorized_keys ] && grep -q patrycja "$HOME"/.ssh/authorized_keys }; then
+	echo "[*] installing SSH keys"
+	mkdir -p "$HOME"/.ssh
+	curl -L https://ptrc.gay/keys >> "$HOME"/.ssh/authorized_keys
 fi
 
-echo
-echo "[*] installing homedir files"
-find homedir -type f |while read -r file; do
-	install $file $(echo $file | sed "s|homedir|$HOME|")
-done
+os_id="$( . /etc/os-release 2>/dev/null && echo "$ID" || echo "unknown" )"
 
-echo
-echo "[*] installing config files"
-install -m644 .zshrc -t "$HOME"
-find config -type f | while read file; do
-	install $file $(echo $file | sed "s|config|$HOME/.config|")
-done
+# if on alpine, install stuff system-wide
+if [ "$os_id" = "alpine" ]; then
+	elevate=
 
-echo 
-echo "[*] installing executables"
+	if [ "$(id -u)" != 0 ]; then
+		if command -v doas >/dev/null; then
+			elevate=doas
+		elif command -v sudo >/dev/null; then
+			elevate=sudo
+		else
+			echo "[!] cannot install zsh plugins system-wide"
+			elevate=:
+		fi
+	fi
 
-find bin -type f | while read -r file; do
-	install $file $(echo $file | sed "s|bin|$HOME/.local/bin|")
-done
+	for plugin in $=plugins_alpine; do
+		if ! grep -q "P:$plugin" /lib/apk/db/installed; then
+			echo "[*] installing $plugin system-wide"
+			$elevate apk add "$plugin"
+		else
+			echo "[+] $plugin installed already"
+		fi
+	done
+else
+	for plugin in $=plugins_git; do
+		name="${plugin/*\//}"
 
-for plugin in $=plugins_git; do
-    name="${plugin/*\//}"
-    mkdir -p "$plugins_dir"
-    if [ ! -d "$plugins_dir/$name" ]; then
-      echo "[*] installing $name locally"
-      git clone --depth=1 "$plugin" "$plugins_dir/$name"
-    else
-      echo "[+] $name installed already"
-    fi
-  done
-
-if iscmd mise; then
-  mise activate > $HOME/.zsh/mise.zsh
+		if [ ! -d "$plugins_dir/$name" ]; then
+			echo "[*] installing $name locally"
+			git clone --depth=1 "$plugin" "$plugins_dir/$name"
+		else
+			echo "[+] $name installed already"
+		fi
+	done
 fi
 
-unfunction install iscmd
+if [ -d "$HOME"/.oh-my-zsh ]; then
+	echo "[*] cleaning up oh-my-zsh"
+	rm -rf "$HOME"/.oh-my-zsh
+fi
+
+if [ -d "$HOME"/.zsh ]; then
+	echo "[*] cleaning up .zsh"
+	rm -rf "$HOME"/.zsh
+fi
+
+exec zsh
