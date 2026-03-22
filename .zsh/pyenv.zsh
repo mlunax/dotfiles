9f956da0d51880e54c12779d703fe4b7814b627e
Luna Komorebi <luna@lunax.dev>
2026-03-22 13:36:25 +0100

    remove old .zsh dir

--- .zsh/pyenv.zsh
+++
@@ -1,5 +0,0 @@
-if [[ -d "$HOME/.pyenv/" ]]; then
-        export PYENV_ROOT="$HOME/.pyenv"
-        command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-        eval "$(pyenv init -)"
-fi
